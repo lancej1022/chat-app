5eb125040ab24a8a7eb974e8d8bcc7389647e567
lancej1022 <46359098+lancej1022@users.noreply.github.com>
2025-04-04 16:05:37 -0700

    Fix some of the missing tailwind classes on `Button` component when consumed within `web` app. Checkboxes are still totally broken.

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -1,18 +1,15 @@
-// import { defineConfig } from "vite";
-// TODO: convert this file to TS -- unclear why TSR CLI generated it as JS
-
-// import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+// TODO: convert this file to TS -- need to remove the vitest stuff
 import { resolve } from "node:path";
-// TODO: does this work with Tailwind@3.x ?
-// import path from "path";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import react from "@vitejs/plugin-react";
+// import tailwindcss from "tailwindcss";
 import { defineConfig, transformWithEsbuild } from "vite";
 import reactNativeWeb from "vite-plugin-react-native-web";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    minify: false,
+  },
   plugins: [
     // TODO: enable SSG / SSR for initial page load
     TanStackRouterVite({ autoCodeSplitting: true }),
@@ -32,6 +29,12 @@ export default defineConfig({
     viteReact(),
     reactNativeWeb(),
   ],
+  // css: {
+  //   postcss: {
+  //     plugins: [tailwindcss()],
+  //   },
+  // },
+  // TODO: this is probably why the file is `.js` instead of `.ts`
   test: {
     globals: true,
     environment: "jsdom",
@@ -52,46 +55,3 @@ export default defineConfig({
     },
   },
 });
-
-// https://vite.dev/config/
-// export default defineConfig({
-//   build: {
-//     sourcemap: true,
-//     outDir: "dist",
-//   },
-//   resolve: {
-//     alias: {
-//       "~": resolve(__dirname, "src"),
-//       "@": resolve(__dirname, "./src"),
-//     },
-//   },
-//   plugins: [
-//     {
-//       name: "treat-js-files-as-jsx",
-//       async transform(code, id) {
-//         if (!id.match(/node_modules\/.*\.(js|mjs|web\.mjs)$/)) {
-//           return null;
-//         }
-
-//         return transformWithEsbuild(code, id, {
-//           loader: "jsx",
-//           jsx: "automatic",
-//         });
-//       },
-//     },
-//     TanStackRouterVite({ autoCodeSplitting: true }),
-//     // TanStackRouterVite({ target: 'react', autoCodeSplitting: true }), // TODO: working version from `chat-app`
-//     react(),
-//     reactNativeWeb(),
-//   ],
-//   optimizeDeps: {
-//     force: true,
-//     esbuildOptions: {
-//       loader: {
-//         ".js": "jsx",
-//         ".mjs": "jsx",
-//         ".web.mjs": "jsx",
-//       },
-//     },
-//   },
-// });
