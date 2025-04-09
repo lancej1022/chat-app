97413ed5ae84e12686229f2af043462be1dc0810
lancej1022 <46359098+lancej1022@users.noreply.github.com>
2025-04-08 23:17:06 -0700

    Update `LoginScreen` in expo app to look more appropriate on mobile

--- apps/expo/src/components/login/login-screen.tsx
+++ apps/expo/src/components/login/login-screen.tsx
@@ -4,29 +4,31 @@ import { Text } from "@chat-app/base-component-lib";
 import { LoginForm } from "@chat-app/features";
 import { GalleryVerticalEnd } from "lucide-react-native";
 
+function Branding() {
+  return (
+    <View
+      // TODO: when this is a `Text` w/ href, it causes the row to sit to the far left on mobile only for some reason
+      // href="#"
+      className="flex flex-row items-center justify-center gap-2 text-center font-medium"
+    >
+      <View className="flex h-6 w-6 flex-row items-center justify-center self-center rounded-md bg-primary text-primary-foreground">
+        {/* TODO: this doesnt appear as white on mobile for some reason? Not sure if its a theme issue*/}
+        <GalleryVerticalEnd className="size-4 text-primary-foreground" />
+      </View>
+      <Text>Chat app</Text>
+    </View>
+  );
+}
+
 export function LoginScreen() {
   return (
-    // <View className="grid min-h-screen lg:grid-cols-2">
-    // <View className="flex min-h-screen flex-col justify-center lg:flex-row">
-    // <View className="min-h-screen">
-    // TODO: this component needs to vertically center the form! But `min-h-screen` causes things to stretch to the bottom on mobile? Yet it seems required for web...?
-    // One potential solution would be having the Web app wrap a full-height view using flexbox around everything, probably on the body or #root.
-    <View>
-      <View className="flex h-full flex-col">
-        <View
-          // TODO: when this is a `Text` w/ link, it causes the row to sit to the far left on mobile only for some reason
-          // href="#"
-          className="flex flex-row items-center justify-center gap-2 text-center font-medium"
-        >
-          <View className="flex h-6 w-6 flex-row items-center justify-center self-center rounded-md bg-primary text-primary-foreground">
-            {/* TODO: this doesnt appear as white on mobile for some reason? */}
-            <GalleryVerticalEnd className="size-4 text-primary-foreground" />
-          </View>
-          <Text>Chat app</Text>
-        </View>
+    <View className="min-h-screen items-center justify-center">
+      <View className="flex flex-col gap-5">
+        <Branding />
+
         <View
           data-testid="login-form-wrap"
-          className="flex flex-shrink flex-grow basis-auto flex-row items-center justify-center"
+          className="flex flex-shrink flex-grow basis-auto flex-row items-baseline justify-center"
         >
           <View className="w-full max-w-xs">
             <LoginForm />
